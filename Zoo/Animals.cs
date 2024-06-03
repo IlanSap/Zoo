9df0ce9aa11bf8c7da6a9537a29726fb2df76eaa
Ilan Sapoznikov <ilansapo@gmail.com>
2024-06-03 17:57:29 +0300

    Separated Animals class into separate classes for each type of animal and placed these classes into a new 'Animals' folder.

--- Zoo/Animals.cs
+++ Zoo/Animals.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+/*
 public class Lion : IAnimal
 {
     public string Name { get; private set; }
@@ -62,4 +62,4 @@ public class Monkey : IAnimal
 
     public ConsoleColor AnimalBackgroundColor { get; } = ConsoleColor.Green;
     public ConsoleColor AnimalForegroundColor { get; } = ConsoleColor.Black;
-}
+}*/
---
+++ Zoo/Animals/Monkey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Monkey : IAnimal
+{
+    public AnimalType AnimalType { get; } = AnimalType.Monkey;
+
+    public List<Tuple<int, int>> MoveDirections { get; } = new List<Tuple<int, int>>()
+    {
+        new Tuple<int, int>(0, 2),
+        new Tuple<int, int>(0, -2),
+        new Tuple<int, int>(-2, 0),
+        new Tuple<int, int>(2, 0)
+    };
+
+    public ConsoleColor AnimalBackgroundColor { get; } = ConsoleColor.Green;
+    public ConsoleColor AnimalForegroundColor { get; } = ConsoleColor.Black;
+}
