81f84ba0075bb566704a2cf3bf11203b59a133ac
Alexander Furer <alexander.furer@intel.com>
2024-07-01 17:23:04 +0300

    DI

--- Zoo/Program.cs
+++ Zoo/Program.cs
@@ -5,46 +5,38 @@ using ZooProject.Data;
 
 namespace ZooProject;
 
-class Program
-{
-    static void Main(string[] args)
-    {
+class Program {
+    static void Main(string[] args) {
         // Setup dependency injection
         var services = new ServiceCollection();
         ConfigureServices(services);
 
         var serviceProvider = services.BuildServiceProvider();
 
-        try
-        {
+        try {
             // Using a scope to resolve scoped services
-            using (var scope = serviceProvider.CreateScope())
-            {
-                var zooService = scope.ServiceProvider.GetRequiredService<ZooService>();
-                var consoleHelper = new ConsoleHelper();
+            using (var scope = serviceProvider.CreateScope()) {
+                var consoleHelper = scope.ServiceProvider.GetRequiredService<ConsoleHelper>();
 
-                ZooManager zooManager = new ZooManager(zooService, consoleHelper);
-                if (consoleHelper.GetRunType() == 1)
-                {
+
+                ZooManager zooManager = scope.ServiceProvider.GetRequiredService<ZooManager>();
+                if (consoleHelper.GetRunType() == 1) {
                     zooManager.Run();
-                }
-                else
-                {
+                } else {
                     zooManager.RunWithComposite();
                 }
             }
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             Console.WriteLine($"An error occurred: {ex.Message}");
         }
     }
 
-    private static void ConfigureServices(IServiceCollection services)
-    {
+    public static void ConfigureServices(IServiceCollection services) {
         services.AddDbContext<ZooContext>(options =>
-            options.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=ZooDB;Trusted_Connection=True; Encrypt=True; TrustServerCertificate=True;"));
+            options.UseSqlServer(
+                @"Server=localhost\SQLEXPRESS;Database=ZooDB;Trusted_Connection=True; Encrypt=True; TrustServerCertificate=True;"));
         services.AddTransient<ZooService>();
-        services.AddTransient<ConsoleHelper>();
+        services.AddSingleton<ConsoleHelper>();
+        services.AddTransient<ZooManager>();
     }
 }
